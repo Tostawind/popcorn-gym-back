0b69f991d4eda660a62e01a28c0c4ebc1f0ff53d
Abel D <adq21791@gmail.com>
2024-04-08 19:24:07 +0200

    feat: basic structure

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Config Express
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+module.exports = app;
